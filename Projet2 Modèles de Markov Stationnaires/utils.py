f724ec06898fb71b542ad1d921fbef7dcbcb523b
yiboe ossee <113049750+osseey@users.noreply.github.com>
2022-10-22 10:06:27 +0200

    Add files via upload

---
+++ Projet2 Modèles de Markov Stationnaires/utils.py
@@ -0,0 +1,107 @@
+import numpy as np
+from math import log2, sqrt
+from scipy.stats import norm
+
+
+def get_line_index(dSize):
+    """
+    Cette fonction permet d'obtenir l'ordre de parcours des pixels d'une image carrée selon un parcours ligne par ligne
+    :param dSize: largeur ou longueur de l'image en pixel (peu importe, la fonction ne fonctionne qu'avec des images carrées)
+    :return: une liste de taille 2*dSize*dSize qui correspond aux coordonnées de chaque pixel ordonnée selon le parcours ligne par ligne
+    """
+    return [a.flatten() for a in np.indices((dSize, dSize))]
+
+
+def line_transform_img(img):
+    """
+    Cette fonction prend une image carrée en entrée, et retourne l'image applatie (1 dimension) selon le parcours ligne par ligne
+    :param img: une image (donc un numpy array 2 dimensions)
+    :return: un numpy array 1 dimension
+    """
+    assert img.shape[0] == img.shape[1], 'veuillez donner une image carrée en entrée'
+    idx = get_line_index(img.shape[0])
+    return img[idx[0], idx[1]]
+
+
+def transform_line_in_img(signal, dSize):
+    """
+    Cette fonction prend un signal 1D en entrée et une taille, et le transforme en image carrée 2D selon le parcours ligne par ligne
+    :param img: un signal 1D
+    :param dSize: largeur ou longueur de l'image en pixel (peu importe, la fonction de fonctionne qu'avec des images carrées)
+    :return: une image (donc un numpy array 2 dimensions)
+    """
+    assert dSize == int(sqrt(signal.shape[0])), 'veuillez donner un signal ayant pour dimension dSize^2'
+    idx = get_line_index(dSize)
+    img = np.zeros((dSize, dSize))
+    img[idx[0], idx[1]] = signal
+    return img
+
+
+def get_peano_index(dSize):
+    """
+    Cette fonction permet d'obtenir l'ordre de parcours des pixels d'une image carrée (dont la dimension est une puissance de 2)
+    selon la courbe de Hilbert-Peano
+    :param dSize: largeur ou longueur de l'image en pixel (peu importe, la fonction de fonctionne qu'avec des images carrées)
+    :return: une liste de taille 2*dSize*dSize qui correspond aux coordonnées de chaque pixel ordonnée selon le parcours de Hilbert-Peano
+    """
+    assert log2(dSize).is_integer(), 'veuillez donne une dimension étant une puissance de 2'
+    xTmp = 0
+    yTmp = 0
+    dirTmp = 0
+    dirLookup = np.array(
+        [[3, 0, 0, 1], [0, 1, 1, 2], [1, 2, 2, 3], [2, 3, 3, 0], [1, 0, 0, 3], [2, 1, 1, 0], [3, 2, 2, 1],
+         [0, 3, 3, 2]]).T
+    dirLookup = dirLookup + np.array(
+        [[4, 0, 0, 4], [4, 0, 0, 4], [4, 0, 0, 4], [4, 0, 0, 4], [0, 4, 4, 0], [0, 4, 4, 0], [0, 4, 4, 0],
+         [0, 4, 4, 0]]).T
+    orderLookup = np.array(
+        [[0, 2, 3, 1], [1, 0, 2, 3], [3, 1, 0, 2], [2, 3, 1, 0], [1, 3, 2, 0], [3, 2, 0, 1], [2, 0, 1, 3],
+         [0, 1, 3, 2]]).T
+    offsetLookup = np.array([[1, 1, 0, 0], [1, 0, 1, 0]])
+    for i in range(int(log2(dSize))):
+        xTmp = np.array([(xTmp - 1) * 2 + offsetLookup[0, orderLookup[0, dirTmp]] + 1,
+                         (xTmp - 1) * 2 + offsetLookup[0, orderLookup[1, dirTmp]] + 1,
+                         (xTmp - 1) * 2 + offsetLookup[0, orderLookup[2, dirTmp]] + 1,
+                         (xTmp - 1) * 2 + offsetLookup[0, orderLookup[3, dirTmp]] + 1])
+
+        yTmp = np.array([(yTmp - 1) * 2 + offsetLookup[1, orderLookup[0, dirTmp]] + 1,
+                         (yTmp - 1) * 2 + offsetLookup[1, orderLookup[1, dirTmp]] + 1,
+                         (yTmp - 1) * 2 + offsetLookup[1, orderLookup[2, dirTmp]] + 1,
+                         (yTmp - 1) * 2 + offsetLookup[1, orderLookup[3, dirTmp]] + 1])
+
+        dirTmp = np.array([dirLookup[0, dirTmp], dirLookup[1, dirTmp], dirLookup[2, dirTmp], dirLookup[3, dirTmp]])
+
+        xTmp = xTmp.T.flatten()
+        yTmp = yTmp.T.flatten()
+        dirTmp = dirTmp.flatten()
+
+    x = - xTmp
+    y = - yTmp
+    return x, y
+
+
+def peano_transform_img(img):
+    """
+    Cette fonction prend une image carrée (dont la dimension est une puissance de 2) en entrée,
+    et retourne l'image applatie (1 dimension) selon le parcours de Hilbert-Peano
+    :param img: une image (donc un numpy array 2 dimensions)
+    :return: un numpy array 1 dimension
+    """
+    assert img.shape[0] == img.shape[1], 'veuillez donner une image carrée en entrée'
+    assert log2(img.shape[0]).is_integer(), 'veuillez donne rune image dont la dimension est une puissance de 2'
+    idx = get_peano_index(img.shape[0])
+    return img[idx[0], idx[1]]
+
+
+def transform_peano_in_img(signal, dSize):
+    """
+    Cette fonction prend un signal 1D en entrée et une taille, et le transforme en image carrée 2D selon le parcours de Hilbert-Peano
+    :param img: un signal 1D
+    :param dSize: largeur ou longueur de l'image en pixel (peu importe, la fonction de fonctionne qu'avec des images carrées)
+    :return: une image (donc un numpy array 2 dimensions)
+    """
+    assert dSize == int(sqrt(signal.shape[0])), 'veuillez donner un signal ayant pour dimension dSize^2'
+    idx = get_peano_index(dSize)
+    img = np.zeros((dSize, dSize))
+    img[idx[0], idx[1]] = signal
+    return img
